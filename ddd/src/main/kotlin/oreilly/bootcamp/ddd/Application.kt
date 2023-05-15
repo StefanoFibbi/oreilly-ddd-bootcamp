4adc210eb91fdfd1a660774932e7a3743b396981
Stefano Fibbi <stefano.fibbi@traderepublic.com>
2023-05-15 13:06:58 +0200

    Ex7: Add a price to the Product

--- ddd/src/main/kotlin/oreilly/bootcamp/ddd/Application.kt
+++ ddd/src/main/kotlin/oreilly/bootcamp/ddd/Application.kt
@@ -1,7 +1,10 @@
 package oreilly.bootcamp.ddd
 
+import oreilly.bootcamp.ddd.domain.Amount
 import oreilly.bootcamp.ddd.domain.Cart
+import oreilly.bootcamp.ddd.domain.Currency
 import oreilly.bootcamp.ddd.domain.Item
+import oreilly.bootcamp.ddd.domain.Price
 import oreilly.bootcamp.ddd.domain.Product
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -14,63 +17,29 @@ fun main(args: Array<String>) {
         Cart()
             .add(
                 itemToAdd = Item(
-                    product = Product(name = "Pen"),
+                    product = Product(
+                        name = "Pen",
+                        price = Price(
+                            amount = Amount(10.toBigDecimal()),
+                            currency = Currency.USD
+                        )
+                    ),
                     quantity = 1,
                 )
             )
             .add(
                 itemToAdd = Item(
-                    product = Product(name = "Rubber"),
+                    product = Product(
+                        name = "Rubber",
+                        price = Price(
+                            amount = Amount(5.50.toBigDecimal()),
+                            currency = Currency.USD
+                        )
+                    ),
                     quantity = 10,
-                )
-            )
-            .add(
-                itemToAdd = Item(
-                    product = Product(name = "Apple pencil"),
-                    quantity = 10,
-                )
-            )
-            .removeAllProduct(
-                Product(name = "Rubber")
-            )
-            .add(
-                itemToAdd = Item(
-                    product = Product(name = "Rubber"),
-                    quantity = 3,
-                )
-            )
-
-    val cart2: Cart =
-        Cart()
-            .add(
-                itemToAdd = Item(
-                    product = Product(name = "Pen"),
-                    quantity = 1,
-                )
-            )
-            .add(
-                itemToAdd = Item(
-                    product = Product(name = "Rubber"),
-                    quantity = 10,
-                )
-            )
-            .add(
-                itemToAdd = Item(
-                    product = Product(name = "Apple pencil"),
-                    quantity = 10,
-                )
-            )
-            .removeAllProduct(
-                Product(name = "Rubber")
-            )
-            .add(
-                itemToAdd = Item(
-                    product = Product(name = "Rubber"),
-                    quantity = 3,
                 )
             )
 
     println("Item in the cart: ${cart.items}")
     println("Removed products: ${cart.removedProducts().map { it.productName }}")
-    println("Are the carts equals? Answer: ${cart == cart2}")
 }
--- ddd/src/main/kotlin/oreilly/bootcamp/ddd/domain/Product.kt
+++ ddd/src/main/kotlin/oreilly/bootcamp/ddd/domain/Product.kt
@@ -1,3 +1,18 @@
 package oreilly.bootcamp.ddd.domain
 
-data class Product(val name: String)
+import java.math.BigDecimal
+
+data class Product(val name: String, val price: Price)
+
+data class Price(val amount: Amount, val currency: Currency)
+
+@JvmInline
+value class Amount(val value: BigDecimal)
+
+sealed class Currency(
+    val name: String,
+    val symbol: String,
+) {
+    object USD : Currency(name = "USD", symbol = "$")
+    override fun toString(): String = name
+}
